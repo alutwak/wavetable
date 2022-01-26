f3a1c4d2324cb04013724374819f6b0ca23214b3
Ayal Lutwak <alutwak@protonmail.com>
2022-01-25 23:48:11 -0800

    wavesynth: synth plays audio with some bugs

--- src/bin/wavesynth/main.rs
+++ src/bin/wavesynth/main.rs
@@ -1,15 +1,45 @@
-use clap::Parser;
-use portmidi::PortMidi;
+use std::sync::Arc;
+use std::sync::mpsc::channel;
 
+use portmidi::PortMidi;
+use clap::Parser;
 
 mod midi;
 mod instrument;
+mod stream;
 use midi::{MidiError, Message};
 use instrument::Instrument;
+use wavetable::system::System;
 use wavetable::wt::Wavetable;
 
 fn main() -> Result<(), i32> {
 
+    let args = Args::parse();
+
+    if let Some(midi_device) = args.midi_device.as_deref() {
+        println!("MIDI Device: {}", midi_device);
+    }
+
+    let system = Arc::new(System::new(args.samplerate as f32, args.bufsize as u64, args.bufsize));
+
+    let table = Wavetable::from_sndfile(&args.wavetable).map_err(
+        |e| {
+            println!("{}", e);
+            1
+        })?;
+
+    let table = Arc::new(table);
+
+    let mut instrument =  Instrument::new(
+        &system,
+        &table,
+        args.voices,
+        args.attack/1000.0,
+        args.decay/1000.0,
+        args.sustain,
+        args.release/1000.0);
+
+    // Create Midi Device
     let pm = PortMidi::new().unwrap();
     let midi_dev_result = midi::get_midi_device(&pm);
     if let Err(err) = midi_dev_result {
@@ -24,48 +54,68 @@ fn main() -> Result<(), i32> {
     }
     let mut mididev = midi_dev_result.unwrap();
 
+    let (tx, rx) = channel::<Message>();
+
+    let perform = move |outbuf: &mut [f32], _: &cpal::OutputCallbackInfo| {
+        instrument.perform(outbuf);
+
+        for msg in rx.try_iter() {
+            instrument.map_midi(&msg);
+        }
+    };
+
+    let _stream = stream::make_stream(&system, perform);
+
     loop {
         if let Some(event) = mididev.read().unwrap() {
-            match midi::map_note(&event.message) {
+            let msg = midi::map_message(&event.message);
+            match msg {
                 Message::NoteOff {chan, note, vel} => {
-                    println!("NoteOff[{}]: chan({}), note({}), vel({})", event.timestamp, chan, note, vel);
+                    println!("NoteOff: chan({}), note({}), vel({})", chan, note, vel);
                 }
                 Message::NoteOn {chan, note, vel} => {
-                    println!("NoteOn[{}]: chan({}), note({}), vel({})", event.timestamp, chan, note, vel);
+                    println!("NoteOn: chan({}), note({}), vel({})", chan, note, vel);
                 }
-                Message::PolyPressure {chan, note, vel} => {
-                    println!("PolyPressure[{}]: chan({}), note({}), vel({})", event.timestamp, chan, note, vel);
-                }
-                Message::ControlChange {chan, ctrl, val} => {
-                    println!("ControlChange[{}]: chan({}), ctrl({}), vel({})", event.timestamp, chan, ctrl, val);
-                }
-                Message::ProgramChange {chan, prog} => {
-                    println!("ProgramChange[{}]: chan({}), prog({})", event.timestamp, chan, prog);
-                }
-                Message::ChannelPressure {chan, vel} => {
-                    println!("ChanPressure[{}]: chan({}), vel({})", event.timestamp, chan, vel);
-                }
-                Message::PitchBend {chan, lsb, msb} => {
-                    println!("PitchBend[{}]: chan({}), pitch({})", event.timestamp, chan, lsb as u16 | (msb as u16) << 8);
-                }
-                _ => {
-                    println!("Event ({}): status ({}): {}, {}, {}",
-                             event.timestamp,
-                             event.message.status,
-                             event.message.data1,
-                             event.message.data2,
-                             event.message.data3);
-
-                }
+                _ => continue
             }
+            tx.send(msg).unwrap();
         }
     }
-
-    Ok(())
 }
 
+#[derive(Parser)]
+#[clap(version = "wavesynth 0.1.0", long_about = None)]
+#[clap(about = "A MIDI-controlled wavetable synthesizer")]
+struct Args {
 
-// struct Args {
+    /// Path to an audio file to use for a wavetable
+    wavetable: String,
 
+    /// Envelope attack, in ms
+    attack: f32,
 
-// }
+    /// Envelope decay, in ms
+    decay: f32,
+
+    /// Envelope sustain, in a range of [0..1]
+    sustain: f32,
+
+    /// Envelope release, in ms
+    release: f32,
+
+    /// Optional MIDI device to use. If not given, then device will be queried
+    #[clap(short, long)]
+    midi_device: Option<String>,
+
+    /// The playback samplerate, in Hz
+    #[clap(short, long, default_value = "48000")]
+    samplerate: usize,
+
+    /// The buffer size to use, in samples
+    #[clap(short, long, default_value = "256")]
+    bufsize: usize,
+
+    /// The maximum number of voices to use
+    #[clap(short, long, default_value = "8")]
+    voices: usize,
+}
